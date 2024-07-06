a5288f376cf35c22660579fe2be5eaba467e6fc3
jenilparmar <jerryhash231@gmail.com>
2024-07-07 00:42:38 +0530

    Corrected map styling

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Origin", value: "192.168.242.141:3000/" }, // specify one origin here
                     { key: "Access-Control-Allow-Origin", value: "192.168.242.141:3000/map" }, // specify one origin here
                     { key: "Access-Control-Allow-Origin", value: "/" }, // specify one origin here
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000/" }, // or use a single string for multiple origins
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // or use a single string for multiple origins
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
