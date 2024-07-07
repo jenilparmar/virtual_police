88f8a539aecefd32615b085ab87020161e3f20f7
jenilparmar <jerryhash231@gmail.com>
2024-07-07 22:28:56 +0530

    Loading effect corrected

--- app/crimereport/page.jsx
+++ app/crimereport/page.jsx
@@ -2,15 +2,19 @@
 import React, { useEffect, useState } from "react";
 import Crime from "@/Components/Crime";
 import axios from "axios";
+import Loader from "@/Components/Loader";
 
 function Page() {
   const [crime, setCrime] = useState([]);
+  const [loaderActive , setLoaderActive] = useState(true)
 
   useEffect(() => {
+
     axios.get("https://virtual-police.vercel.app/api/getallData")
   
       .then(res => {
         const data = res.data;
+        setLoaderActive(true)
         setCrime(data);
         // console.log(crime); // Log the data directly received from the API
       })
@@ -20,14 +24,14 @@ function Page() {
   }, []);
 
   return (
-    <section className="text-gray-400 bg-gray-900 body-font h-screen overflow-hidden">
+    loaderActive?<Loader/>:<section className="text-gray-400 bg-gray-900 body-font h-screen overflow-hidden">
       <h1 className="relative top-10 text-xl font-bold text-blue-400 text-center">
         Crime Reports
       </h1>
       <div className="h-1 w-10/12 self-center bg-blue-400 rounded-sm relative top-12 left-8"></div>
       <br />
       <br />
-      {crime.map((crm, idx) => (
+     {crime.map((crm, idx) => (
         <Crime
           key={idx}
           crime={crm.crime}
